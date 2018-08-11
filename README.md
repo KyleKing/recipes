6ad908bf0537ea7a51e7c44d828b3ce6a9d0337d
Kyle King <kmking72@gmail.com>
2018-08-11 10:22:45 -0400

    Refac: add linting & Reorganize src dir

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Recipes
 
-Run `python make.py` to create an output index.html file based on the source markdown files in the src/ directory
+Create interactive website for searching and making lists from user-specified source files
+
+## Workflow
+
+1. Create <recipe_title>.json <recipe_title>.<img_format> in respective database directory. See template for all options.
+2. Run `python make.py`
+    - Move source images to `dist/imgs/<subdir>-<recipe_title>.<img_format>`
+    - Combine JSON documents into single JSON file
+        + Add recipe title as title case of filename split on underscores
+        + Adds link to minified image
+        + Standardizes ingredients
+        + Identifies errors in source file
+3. Open index.html in a browser or push to Github and view at https://kyleking.me/recipes/
+
+## Libraries
+
+- Crel for DOM creation: https://github.com/KoryNunn/crel
+- Fuse for fuzzy-search: http://fusejs.io/
+
+### Cool, but didn't use
+
+- Bearhug: https://github.com/jharding/bearhug
--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,33 @@
-jQuery( document ).ready( function( $ ) {
+jQuery( document ).ready( ( $ ) => {
   //
   // Slide in Side Navigation inspired by CodyHouse article:
   //
 
   function toggleNav( bool ) {
-    $( '.cd-nav-container, .cd-overlay' ).toggleClass( 'is-visible', bool );
-    $( 'main' ).toggleClass( 'scale-down', bool );
+    $( '.cd-nav-container, .cd-overlay' ).toggleClass( 'is-visible', bool )
+    $( 'main' ).toggleClass( 'scale-down', bool )
   }
   // Open navigation clicking the menu icon
-  $( '.cd-nav-trigger' ).on( 'click', function( event ) {
-    event.preventDefault();
-    toggleNav( true );
-  } );
+  $( '.cd-nav-trigger' ).on( 'click', ( event ) => {
+    event.preventDefault()
+    toggleNav( true )
+  } )
   // Close the navigation
-  $( '.cd-close-nav, .cd-overlay' ).on( 'click', function( event ) {
-    event.preventDefault();
-    toggleNav( false );
-  } );
+  $( '.cd-close-nav, .cd-overlay' ).on( 'click', ( event ) => {
+    event.preventDefault()
+    toggleNav( false )
+  } )
   // Select a new section
   $( '.cd-nav li' ).on( 'click', function() {
-    var target = $( this );
+    var target = $( this )
     // var sectionTarget = target.data('menu');
     if ( !target.hasClass( 'cd-selected' ) ) {
       // if user has selected a section different from the one alredy visible
       // update the navigation -> assign the .cd-selected class to the selected item
-      target.addClass( 'cd-selected' ).siblings( '.cd-selected' ).removeClass( 'cd-selected' );
+      target.addClass( 'cd-selected' ).siblings( '.cd-selected' ).removeClass( 'cd-selected' )
     }
-    toggleNav( false );
-  } );
+    toggleNav( false )
+  } )
 
 
   //
@@ -36,77 +36,77 @@ jQuery( document ).ready( function( $ ) {
 
   function parseURL( fullUrl ) {
     // Initialize URL components for export
-    var urlComps = new Object();
-    urlComps.tagBR = '\?tag=';
-    urlComps.ingBR = '\?ingredients=';
+    var urlComps = new Object()
+    urlComps.tagBR = '\?tag='
+    urlComps.ingBR = '\?ingredients='
     urlComps.ingredients = []
     urlComps.tag = ''
     // Separate ingredient list from rest of URL
-    var components = fullUrl.split( '\?' );
+    var components = fullUrl.split( '\?' )
     for ( var idx = 0; idx < components.length; idx ++ ) {
-      var comp = '?' + components[idx];
+      var comp = '?' + components[idx]
       if ( comp.indexOf( urlComps.ingBR ) >= 0 ) {
         // Create list of ingredients from URL
-        urlComps.ingredients = comp.split( urlComps.ingBR )[1].split( ',' );
+        urlComps.ingredients = comp.split( urlComps.ingBR )[1].split( ',' )
         // console.log('urlComps.ingredients: ' + urlComps.ingredients);
         // console.log(comp.split(urlComps.ingBR));
       } else if ( comp.indexOf( urlComps.tagBR ) >= 0 ) {
         // Parse the linked recipe tag
-        urlComps.tag = comp.split( urlComps.tagBR )[1];
+        urlComps.tag = comp.split( urlComps.tagBR )[1]
         // console.log('urlComps.tag: ' + urlComps.tag);
         // console.log(comp.split(urlComps.tagBR));
       } else {
         // FIXME: Without the tag, the URL can't be updated...
-        urlComps.baseUrl = comp.slice( 1 ).split( '#' )[0] + '#';
+        urlComps.baseUrl = comp.slice( 1 ).split( '#' )[0] + '#'
         // console.log('urlComps.baseUrl: ' + urlComps.baseUrl);
       }
     }
-    return( urlComps );
+    return( urlComps )
   }
 
   // Check item when clicked
   //  open navigation clicking the menu icon
-  $( '.ingredient' ).on( 'click', function( event ) {
+  $( '.ingredient' ).on( 'click', ( event ) => {
     // For debugging:
     var fullUrl = window.location.href
-    if ( fullUrl.search( 'Users/kyleking' ) ) {
-      console.log( 'Current url: ' + fullUrl );
+    if ( fullUrl.search( 'Users/kyleking' ) )
+      console.log( 'Current url: ' + fullUrl )
       // console.log(event);
       // console.log(event.target.classList);
-    }
+
     // Either add or remove ingredient ID and set check box value appropriately
-    var comps = parseURL( fullUrl );
-    var ingID = $( event.target ).attr( 'id' );
+    var comps = parseURL( fullUrl )
+    var ingID = $( event.target ).attr( 'id' )
     if ( event.currentTarget.checked ) {
-      if ( comps.ingredients.length === 0 ) {
-        comps.ingredients = [ingID];
-      } else {
-        comps.ingredients.push( ingID );
-      }
+      if ( comps.ingredients.length === 0 )
+        comps.ingredients = [ingID]
+      else
+        comps.ingredients.push( ingID )
+
     } else {
       // console.log('idx:');
       // console.log(ingID);
       // console.log(comps.ingredients);
       // console.log(comps.ingredients.indexOf(ingID));
-      comps.ingredients.splice( comps.ingredients.indexOf( ingID ), 1 );
+      comps.ingredients.splice( comps.ingredients.indexOf( ingID ), 1 )
     }
-    comps.ingredients.sort();
+    comps.ingredients.sort()
     var finalUrl = comps.baseUrl + comps.ingBR + comps.ingredients.join( ',' ) +
-                   comps.tagBR + comps.tag;
-    window.history.pushState( null, null, finalUrl );
-    console.log( 'Final url: ' + finalUrl + '\n' );
-  } );
+                   comps.tagBR + comps.tag
+    window.history.pushState( null, null, finalUrl )
+    console.log( 'Final url: ' + finalUrl + '\n' )
+  } )
 
   // Read ingredients state and update check boxes:
-  var fullUrl = window.location.href;
-  var ingredients = parseURL( fullUrl ).ingredients;
+  var fullUrl = window.location.href
+  var ingredients = parseURL( fullUrl ).ingredients
   for ( var idx = 0; idx < ingredients.length; idx++ ) {
-    var ingID  = ingredients[idx];
+    var ingID  = ingredients[idx]
     if ( fullUrl.search( 'file:/' ) ) {
-      console.log( 'Toggling? #' + ingID );
-      console.log( $( '#' + ingID ) );
+      console.log( 'Toggling? #' + ingID )
+      console.log( $( '#' + ingID ) )
     }
-    $( '#' + ingID ).prop( 'checked', true );
+    $( '#' + ingID ).prop( 'checked', true )
   }
 
 
@@ -115,26 +115,26 @@ jQuery( document ).ready( function( $ ) {
   //
 
   // Identify and scroll to the linked recipe
-  var anchor = parseURL( window.location.href ).tag;
-  console.log( 'Scrolling to anchor: ' + anchor );
+  var anchor = parseURL( window.location.href ).tag
+  console.log( 'Scrolling to anchor: ' + anchor )
   if ( anchor.length > 0 ) {
-    console.log( 'Scrolling!' );
-    document.getElementById( anchor ).scrollIntoView( {behavior: 'smooth', block: 'start'} );
+    console.log( 'Scrolling!' )
+    document.getElementById( anchor ).scrollIntoView( {behavior: 'smooth', block: 'start'} )
   }
   // Over-ride internal HTML linking from <a> tags
-  $( 'a' ).on( 'click', function( event ) {
-    var tag = event.target.hash.replace( '#', '' );  // or use <str>.slice(1);
+  $( 'a' ).on( 'click', ( event ) => {
+    var tag = event.target.hash.replace( '#', '' )  // or use <str>.slice(1);
     // // If the id is for the cody house panel, ignore (e.g. cd-nav, cd-.*, etc.)
     // if (tag.indexOf('cd-') != 0) {
     // Only use for recipe links
-    if ( tag.indexOf( 'recipe-' ) == 0 ) {
-      event.preventDefault();
-      var comps = parseURL( window.location.href );
+    if ( tag.indexOf( 'recipe-' ) === 0 ) {
+      event.preventDefault()
+      var comps = parseURL( window.location.href )
       var finalUrl = comps.baseUrl + comps.ingBR + comps.ingredients.join( ',' ) +
-                     comps.tagBR + tag.replace( ' ', '_' );
-      console.log( 'Scrolling to new tag: ' + tag + ' for finalUrl: ' + finalUrl );
-      document.getElementById( tag ).scrollIntoView( {behavior: 'smooth', block: 'start'} );
-      window.history.pushState( null, null, finalUrl );
+                     comps.tagBR + tag.replace( ' ', '_' )
+      console.log( 'Scrolling to new tag: ' + tag + ' for finalUrl: ' + finalUrl )
+      document.getElementById( tag ).scrollIntoView( {behavior: 'smooth', block: 'start'} )
+      window.history.pushState( null, null, finalUrl )
     }
-  } );
-} );
+  } )
+} )
