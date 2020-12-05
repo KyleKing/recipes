8bcbafd70845f2059f7635fce70f2c326276a9d0
Kyle King <KyleKing@users.noreply.github.com>
2020-12-05 14:14:33 -0500

    New: intialize python package

--- dodo.py
+++ dodo.py
@@ -18,15 +18,14 @@ poetry run doit
 
 """
 
-import webbrowser
 from pathlib import Path
 
 from dash_dev import LOGGER_CONFIG
-from dash_dev.doit_helpers.base import debug_task
-from dash_dev.doit_helpers.doit_globals import DIG, DoItTask
+from dash_dev.doit_helpers.doit_globals import DIG
 from dash_dev.registered_tasks import *  # noqa: F401,F403,H303 skipcq: PYL-W0614 (Run 'doit list' to see tasks)
-from doit.tools import LongRunning
 from loguru import logger
+
+from recipes.tasks import task_deploy, task_main, task_serve  # noqa: F401I
 
 logger.configure(**LOGGER_CONFIG)
 logger.info('Starting DoIt tasks in dodo.py')
@@ -42,43 +41,10 @@ DOIT_CONFIG = {
         # 'coverage',
         # # 'open_test_docs',
         'set_lint_config',
-        'create_tag_file',  # FIXME: this should ignore 'NO_TAG_SUM' and gitignore (i.e. /site/*)
+        # 'create_tag_file',  # FIXME: this should ignore 'NO_TAG_SUM' and gitignore (i.e. /site/*)
         'auto_format',
         'lint_pre_commit',
         'deploy',
     ],
 }
 """DoIt Configuration Settings. Run with `poetry run doit`."""
-
-
-def task_main() -> DoItTask:
-    """Format markdown files.
-
-    Returns:
-        DoItTask: DoIt task
-
-    """
-    return debug_task([LongRunning('poetry run python main.py')])
-
-
-def task_serve() -> DoItTask:
-    """Serve the recipe site with `--dirtyreload` and open in a web browser.
-
-    Returns:
-        DoItTask: DoIt task
-
-    """
-    return debug_task([
-        (webbrowser.open, ('http://localhost:8000', )),
-        LongRunning('poetry run mkdocs serve --dirtyreload'),
-    ])
-
-
-def task_deploy() -> DoItTask:
-    """Deploy to Github `gh-pages` branch.
-
-    Returns:
-        DoItTask: DoIt task
-
-    """
-    return debug_task([LongRunning('poetry run mkdocs gh-deploy')])
---
+++ recipes/tasks.py
@@ -0,0 +1,40 @@
+"""Doit Tasks for the MKDocs project."""
+
+import webbrowser
+
+from dash_dev.doit_helpers.base import debug_task
+from dash_dev.doit_helpers.doit_globals import DoItTask
+from doit.tools import LongRunning
+
+
+def task_main() -> DoItTask:
+    """Format markdown files.
+
+    Returns:
+        DoItTask: DoIt task
+
+    """
+    return debug_task([LongRunning('poetry run python main.py')])
+
+
+def task_serve() -> DoItTask:
+    """Serve the recipe site with `--dirtyreload` and open in a web browser.
+
+    Returns:
+        DoItTask: DoIt task
+
+    """
+    return debug_task([
+        (webbrowser.open, ('http://localhost:8000', )),
+        LongRunning('poetry run mkdocs serve --dirtyreload'),
+    ])
+
+
+def task_deploy() -> DoItTask:
+    """Deploy to Github `gh-pages` branch.
+
+    Returns:
+        DoItTask: DoIt task
+
+    """
+    return debug_task([LongRunning('poetry run mkdocs gh-deploy')])
