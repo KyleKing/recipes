baf25d4277294ce9df7182ce0d9b4be3addc5cce
Kyle King <KyleKing@users.noreply.github.com>
2024-12-28 11:37:26 -0500

    feat: switch to go-installed minifier

--- build.sh
+++ build.sh
@@ -5,7 +5,11 @@ cp -R docs/ public/
 
 node build.js
 
+# Using: github.com/tdewolff/minify/v2/cmd/minify
+minify --recursive --output ./ ./public
+
 rm public/_icons/*.sketch
+
 cp CNAME public/CNAME
 
 # https://github.com/c-w/ghp-import/blob/5219f00fc83606ff426b978a9920ea746923dcb7/ghp_import.py#L157-L164
