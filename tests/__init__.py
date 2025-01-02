bc92dcf5160025652e0a0263126d18bbf68f7ab1
Kyle King <KyleKing@users.noreply.github.com>
2025-01-01 19:33:12 -0500

    refactor: remove remaining functional code

--- tests/__init__.py
+++
@@ -1,5 +0,0 @@
-import sys
-from os import environ, getenv
-
-DEF_MODE = 'ERROR' if sys.version_info >= (3, 10) else 'WARNING'
-environ['RUNTIME_TYPE_CHECKING_MODE'] = getenv('RUNTIME_TYPE_CHECKING_MODE', DEF_MODE)
