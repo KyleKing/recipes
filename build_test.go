4d028804ddb763c9dbff38b87670bc75d3258188
Kyle King <KyleKing@users.noreply.github.com>
2025-01-03 14:22:02 -0500

    test: initialize test

--- build_test.go
+++ build_test.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestToTitleCase(t *testing.T) {
-	if f := ToTitleCase("words__word_phrase"); f != "Words Word Phrase" {
-		t.Errorf("ToTitleCase(...) = %s", f)
-	}
+	title := ToTitleCase("words__word_phrase")
+	assert.Equal(t, title, "Words Word Phrase")
+}
+
+func TestBuildHtml(t *testing.T) {
+	html, err := BuildHtml("_recipe_template.dj")
+	require.Nil(t, err)
+	assert.Contains(t, html, "PLACEHOLDER")
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,14 @@ module github.com/KyleKing/recipes
 
 go 1.23.4
 
-require github.com/sivukhin/godjot v1.0.5
+require (
+	github.com/sivukhin/godjot v1.0.5
+	github.com/stretchr/testify v1.8.4
+)
 
-require github.com/a-h/templ v0.3.819 // indirect
+require (
+	github.com/a-h/templ v0.3.819
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
