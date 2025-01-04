5ca3c8a8b878bf60901e5f8294a5e5cd87049c62
Kyle King <KyleKing@users.noreply.github.com>
2025-01-04 18:48:43 -0500

    refactor: replace CTS/E formatting with djot attributes

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-find_and_replace() {
-  for file in *dj; do
-      new_content=$(cat "$file" | sed 's/::::::/\n:::\n:::/')
-  echo -e "$new_content" > "$file"
-  done
-}
-
 fd --extension=dj | sad --commit --flags=M '\{\% \[cts\] rating=(\d); \([^\%]+\%}\n+.+\n+\{\% \[cte\] \%}\n+\{\% \[cts\] name_image=([^;]+); \([^\%]+\%}\n+.+\n+\{\% \[cte\] \%}' '{ rating=$1 image="$2" }::::::'
 
-find_and_replace
+python - << EOT
+from pathlib import Path
+
+for pth in Path.cwd().rglob("*.dj"):
+    print(pth)
+    new = pth.read_text().replace("::::::", "\n:::\n:::")
+    pth.write_text(new)
+EOT
