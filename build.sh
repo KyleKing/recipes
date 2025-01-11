0bfd2cfb8b46bf7f814734cacbb6c06ca1446456
Kyle King <KyleKing@users.noreply.github.com>
2025-01-11 15:04:07 -0500

    refactor: remove unused icons and Sketch file

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 # Prepare public directory
 rm -rf public/
 cp -R content/ public/
-rm public/_icons/*.sketch
 cp CNAME public/CNAME
 
 # https://github.com/c-w/ghp-import/blob/5219f00fc83606ff426b978a9920ea746923dcb7/ghp_import.py#L157-L164
