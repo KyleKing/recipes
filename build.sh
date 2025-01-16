82fdb8e21987179d3b9bda8dec37d7f914fcc7e5
Kyle King <KyleKing@users.noreply.github.com>
2025-01-15 19:54:46 -0500

    ci: remove generated file from VCS

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cp CNAME public/CNAME
 # https://github.com/c-w/ghp-import/blob/5219f00fc83606ff426b978a9920ea746923dcb7/ghp_import.py#L157-L164
 touch public/.nojekyll
 
-# Generate HTML from djot recipes
+# Run the first party code
 go run .
 
 # Minify web files with: github.com/tdewolff/minify/v2/cmd/minify
